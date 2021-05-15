65423a023fd25fa344277e7dc7a553116a22b8bd
Vignesh I <i.vigneshdavid246@gmail.com>
2021-05-15 07:47:04 +0530

    Backtracking Template added

--- ratmaze.cpp
+++ ratmaze.cpp
@@ -1,6 +1,8 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+int xpath[2] = {0,1};
+int ypath[2] = {1,0};
 void printpath(bool tracker[][100], pair<int,int> dest) {
     for(int i=0; i<=dest.first; i++) {
         for(int j=0; j<=dest.second; j++) {
@@ -25,13 +27,19 @@ bool trackpath(char maze[][100], bool tracker[][100], pair<int,int> source, pair
         return true;
     }
     tracker[source.first][source.second] = true;
-    bool rightmost = trackpath(maze, tracker, {source.first,source.second+1},dest);
-    if(rightmost) 
-        return true;
-    bool down = trackpath(maze, tracker, {source.first+1,source.second},dest);
+    // bool rightmost = trackpath(maze, tracker, {source.first,source.second+1},dest);
+    // if(rightmost) 
+    //     return true;
+    // bool down = trackpath(maze, tracker, {source.first+1,source.second},dest);
+    // if(down)
+    //     return true;
+    for(int i=0; i<2; i++) {
+        int newx = xpath[i] + source.first;
+        int newy = ypath[i] + source.second;
+        if(trackpath(maze, tracker, {newx,newy}, dest))
+            return true;
+    }
     tracker[source.first][source.second] = false;
-    if(down)
-        return true;
     return false;
 
 
